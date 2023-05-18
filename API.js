5914596b24ddaed3fc8e20248f905349cabf07eb
Victor Mesnerovicz <1victoraugustom@gmail.com>
2023-05-18 00:55:46 -0300

    Otimização de Código e Troca de Idioma Geral

--- API.js
+++ API.js
@@ -10,30 +10,18 @@ $('#buscainfo').on('click', function(e) {
         success: function(result) {
             console.log(result);
             comida = result['items'];
-            nome = comida[0]['name'];
-            document.getElementById("nomeing").innerHTML = nome;
-            calories = comida[0]['calories'];
-            document.getElementById("calories").innerHTML = calories;
-            carbo = comida[0]['carbohydrates_total_g'];
-            document.getElementById("carbot").innerHTML = carbo;
-            coles = comida[0]['cholesterol_mg'];
-            document.getElementById("coles").innerHTML = coles;
-            gords = comida[0]['fat_saturated_g'];
-            document.getElementById("gords").innerHTML = gords;
-            gordt = comida[0]['fat_total_g'];
-            document.getElementById("gordt").innerHTML = gordt;
-            fibra = comida[0]['fiber_g'];
-            document.getElementById("fibra").innerHTML = fibra;
-            pot = comida[0]['potassium_mg'];
-            document.getElementById("pot").innerHTML = pot;
-            prot = comida[0]['protein_g'];
-            document.getElementById("prot").innerHTML = prot;
-            serve = comida[0]['serving_size_g'];
-            document.getElementById("serve").innerHTML = serve;
-            sod = comida[0]['sodium_mg'];
-            document.getElementById("sóÓdio").innerHTML = sod;
-            doce = comida[0]['sugar_g'];
-            document.getElementById("doce").innerHTML = doce;
+            document.getElementById("nomeing").innerHTML = comida[0]['name'];
+            document.getElementById("calories").innerHTML = comida[0]['calories'];
+            document.getElementById("carbot").innerHTML = comida[0]['carbohydrates_total_g']+" g";
+            document.getElementById("coles").innerHTML = comida[0]['cholesterol_mg']+" mg";
+            document.getElementById("gords").innerHTML = comida[0]['fat_saturated_g']+" g";
+            document.getElementById("gordt").innerHTML = comida[0]['fat_total_g']+" g";
+            document.getElementById("fibra").innerHTML = comida[0]['fiber_g']+" g";
+            document.getElementById("pot").innerHTML = comida[0]['potassium_mg']+" mg";
+            document.getElementById("prot").innerHTML =   comida[0]['protein_g']+" g";
+            document.getElementById("serve").innerHTML = comida[0]['serving_size_g']+" g";
+            document.getElementById("sóÓdio").innerHTML = comida[0]['sodium_mg']+" mg";
+            document.getElementById("doce").innerHTML = comida[0]['sugar_g']+" g";
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
